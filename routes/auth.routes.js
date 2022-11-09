a44e3f3099d9b0c91e045b9b758878d01b37767e
Renata Inojosa <inojosa.renata@gmail.com>
2022-11-08 22:14:56 -0300

    criada rota de login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose')
 
 const User = require('../models/User.model');
+const { isAuthenticated } = require('../middlewares/jwt.middleware')
 
 const saltRounds = 10;
 
@@ -47,6 +48,51 @@ router.post('/signup', async (req, res, next) => {
         }
         next(error);
     }
-})
+});
+
+router.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json('Campos de email e senha obrigatórios!')
+        }
+        const userFromDB = await User.findOne({ email });
+
+        if(!userFromDB) {
+            return res.status(401).json('Usuário ou senha não encontrado!');
+        }
+
+        const verify = bcrypt.compareSync(password, userFromDB.passwordHash);
+
+        if(!verify) {
+            return res.status(401).json('Usuário ou senha não encontrado!')
+        }
+
+        const payload = {
+            _id: userFromDB._id,
+            username: userFromDB.username,
+            email: userFromDB.email,
+            contact: userFromDB.contact,
+        };
+
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {
+                algorithm: "HS256",
+                expiresIn: '3d'
+            }
+        )
+
+        res.status(200).json({ token });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/verify', isAuthenticated, (req, res, next) => {
+    res.status(200).json(req.payload)
+});
 
 module.exports = router;
