45a6bad887b19089005d927c95f386e3bcd178e1
Renata Inojosa <inojosa.renata@gmail.com>
2022-11-24 12:44:23 -0300

    pull na main

--- routes/auth.ong.routes.js
+++ routes/auth.ong.routes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
-// redeploy
+
 router.get("/ong", isAuthenticated, async (req, res, next) => {
   try {
     const {_id} = req.payload;
