6f8e0d030fc80f0109ead776830e6b4a79856285
Walison Abram <abramwhas@gmail.com>
2022-12-06 19:35:46 -0300

    teste user detail2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,10 +18,10 @@ router.get("/users", async (req, res, next) => {
   }
 });
 
-router.get("/:userId", isAuthenticated, async (req, res, next) => {
-  const {userId} = req.params
-  console.log(userId)
-});
+// router.get("/:userId", isAuthenticated, async (req, res, next) => {
+//   const {userId} = req.params
+//   console.log(userId)
+// });
 // router.get("/:userId", isAuthenticated, async (req, res, next) => {
 //   const {userId} = req.params
 //   try {
