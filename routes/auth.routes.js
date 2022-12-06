205d63f0173f66fc7baa59a68843ef802626527c
Walison Abram <abramwhas@gmail.com>
2022-12-05 23:39:31 -0300

    redeploy teste

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,15 +18,15 @@ router.get("/users", async (req, res, next) => {
   }
 });
 
-router.get("/:userId", isAuthenticated, async (req, res, next) => {
-  const {userId} = req.params
-  try {
-    const userFromDB = await User.findById(userId, { passwordHash: 0, _id: 0});
-    res.status(200).json(userFromDB);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get("/:userId", isAuthenticated, async (req, res, next) => {
+//   const {userId} = req.params
+//   try {
+//     const userFromDB = await User.findById(userId, { passwordHash: 0, _id: 0});
+//     res.status(200).json(userFromDB);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 //redeploy
 router.get("/user", isAuthenticated, async (req, res, next) => {
   try {
